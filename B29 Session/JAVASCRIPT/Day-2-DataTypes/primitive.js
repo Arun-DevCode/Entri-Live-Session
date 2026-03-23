274fde45c85202d050c5c5b582b100ef78217308
Arun-DevCode <arunkumar.d@entri.me>
2026-03-23 09:30:16 +0530

    updated

---
+++ B29 Session/JAVASCRIPT/Day-2-DataTypes/primitive.js
@@ -0,0 +1,30 @@
+
+// JavaScript Primitive Types
+
+
+// 1. Number → Represents numeric values (integers & decimals)
+let age = 25; // integer
+let price = 99.99; // float number
+
+// 2. String → Represents text (single or double quotes)
+let name = "test user";
+let greeting = "Hello World";
+
+// 3. Boolean → Represents true or false values
+let isLoggedIn = true;
+let isAdmin = false;
+
+// 4. Undefined → Variable declared but not assigned a value
+let value;
+console.log(value); // undefined
+
+// 5. Null → Represents intentional empty value
+let data = null;
+
+// 6. BigInt → Used for very large numbers 
+let bigNumber = 123456789012345678901234567890n;
+
+// 7. Symbol → Unique and immutable value
+let uniqueId = Symbol("id");
+
+
