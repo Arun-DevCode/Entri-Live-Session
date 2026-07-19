0883d5face7618346a2bb50ba531e5565fa10047
Arun-DevCode <arunkumar.d@entri.me>
2026-07-19 11:32:05 +0530

    Code updated

---
+++ B47 Session/JAVASCRIPT/script.js
@@ -0,0 +1,52 @@
+// Arithmetic Operators
+
+// + 
+let a = 10
+let b = 20;
+console.log(a+b)
+
+// -
+console.log(b-a)
+
+// * - Ask
+console.log(a*b);
+
+// / - Divide
+console.log(a/b)
+
+// % - Modulus
+console.log(a%b);
+
+// ** - Exp
+console.log(a**2);
+
+
+// Assignment Operators
+
+// 1. Assign 
+var mark = 0;
+
+// 2. Incremental Assignment
+let score = 80
+
+let total = 100;
+
+// total += mark; // +
+// console.log(total);
+// total -=mark
+// console.log(total);
+// total/=mark
+
+console.log(total);
+
+
+// Comparison Operators
+
+// Equal
+console.log(5==10)
+console.log(10==10);
+console.log(5=='5')
+
+// Strict Equal 
+console.log(5==='5')
+console.log(5===15)
