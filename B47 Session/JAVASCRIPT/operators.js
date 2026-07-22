fd9cf7da98fa318ec123d9b545f32f1331c60af8
Arun-DevCode <arunkumar.d@entri.me>
2026-07-22 10:16:18 +0530

    Code Updated

---
+++ B47 Session/JAVASCRIPT/operators.js
@@ -0,0 +1,33 @@
+// // // Logical Operator : &&-AND , || - Pipe , ! - NOT
+
+// // console.log(5==10)
+
+// // console.log(10==10&&155==15);
+
+// // console.log(1000==100 || 1550==1550);
+
+// // console.log(5>10 || 5<10 || 5==10);
+
+// // console.log(5!=="10")
+
+// // const isToday = true;
+
+// console.log(5+1)
+// let value = 10
+// value++
+// value--
+// ++value
+// --value
+// console.log(value)
+
+// let x = 10
+// console.log(typeof x)
+// console.log(typeof 'A')
+// console.log(typeof true);
+// console.log(typeof {})
+
+// const typedResult = typeof {};
+
+// console.log(!typeof typeof {});
+
+console.log(85 === 35 ? "Pass" : 50 == 65 ? "B" : "Not Mentioned");
